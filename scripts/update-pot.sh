7745a6d1f5329c3d3ccac1d0faf33d46106cf97a
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-05-22 06:42:21 +0200

    update translations

--- scripts/update-pot.sh
+++ scripts/update-pot.sh
@@ -10,14 +10,14 @@ echo "Generating 'messages.pot'..."
 xgettext --from-code=UTF-8 \
          --add-comments=Translators \
          --copyright-holder="Lorenzo Carbonell" \
-         --package-name="battery-status@atareao.es" \
+         --package-name="translate-assistant@atareao.es" \
          --output=po/messages.pot \
          -- *.js schemas/*.xml
 
 #Replace some lines of the header with our own
 sed -i '1s/.*/# <LANGUAGE> translation for the Battery Status GNOME Shell Extension./' po/messages.pot
 sed -i "2s/.*/# Copyright (C) $(date +%Y) Lorenzo Carbonell/" po/messages.pot
-sed -i '10s/.*/"Report-Msgid-Bugs-To: https://github.com/atareao/battery-status/issues\n"' po/messages.pot
+sed -i '10s/.*/"Report-Msgid-Bugs-To: https:\/\/github.com\/atareao\/translate-assistant\/issues\n/"' po/messages.pot
 sed -i '17s/CHARSET/UTF-8/' po/messages.pot
 
 echo "'messages.pot' generated!"
